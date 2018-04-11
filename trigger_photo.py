54ec778c73ad8689da4b8f5d2fa1a05dedd910f4
Sven Nelson <bakuhatsu@users.noreply.github.com>
2018-04-11 10:40:18 -0500

    Temporary fix for loss of pyserial support
    The current version of FarmBot lost support for pyserial.  This is a temporary workaround that relies on a sleep call instead of waiting until it hears back from the second raspberry pi.  This code will be reverted to the original once the next version of FarmBot is released with pyserial.

--- trigger_photo.py
+++ trigger_photo.py
@@ -4,21 +4,27 @@ Sven Nelson
 RootBot Farmware to send and recieve serial to/from 2nd raspberry pi
 """
 # Need this for sending serial data
-import serial as s
+#import serial as s
 from time import sleep 
 
 # Open serial connection with second raspberry pi
-ser = s.Serial("/dev/ttyS0", 115200) # Open port
+#ser = s.Serial("/dev/ttyS0", 115200) # Open port
 
 # Send "ON" signal to initiate photo taking process
-ser.write("ON".encode())
+#ser.write("ON".encode())
 
 # Check to see if the CameraPi has sent "OFF" to confirm photography complete
-data = ser.read(8) # read up to 32 bytes (set to 8 bytes)
+#data = ser.read(8) # read up to 32 bytes (set to 8 bytes)
 
-complete = False
-while not complete:
-    if data is not None and "OFF" in data:
-      complete = True
-    else:
-      data = ser.read(8)
+#complete = False
+#while not complete:
+#    if data is not None and "OFF" in data:
+#      complete = True
+#    else:
+#      data = ser.read(8)
+
+#### Temporary fix until FarmBot adds back support for pyserial in Farmware ####
+# Send "ON" signal to initiate photo taking process
+print("RootBotCamPiON")  # make this print something unique (serial sends debugging text)
+# Wait for second pi to finish its tasks.
+sleep(10)

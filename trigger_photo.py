c0d45a1e9b550a75b79ebf0a96d2059b4afbf1e4
Sven Nelson <bakuhatsu@users.noreply.github.com>
2019-10-27 20:07:06 -0700

    Update trigger_photo.py
    Working version

--- trigger_photo.py
+++ trigger_photo.py
@@ -8,14 +8,14 @@ RootBot Farmware to send and recieve serial to/from 2nd raspberry pi
 
 from farmware_tools import device
 # Need this for sending serial data
-#####import serial as s # Looks like FarmBot has removed serial again??? Switch to print statements 10/7/2019
+import serial as s # Looks like FarmBot has removed serial again??? Switch to print statements 10/7/2019
 #from time import sleep # Does not appear necessary anymore
 
 # Open serial connection with second raspberry pi
-#####ser = s.Serial("/dev/ttyAMA0", 115200, timeout=10) # Open port (was ttyS0), gave it a 10 sec timeout if not enough bytes read.
+ser = s.Serial("/dev/ttyAMA0", 115200, timeout=10) # Open port (was ttyS0), gave it a 10 sec timeout if not enough bytes read.
 
 # Send "ON" signal to initiate photo taking process
-#####ser.write("RootCamONRootCamON".encode())
+ser.write("RootCamONRootCamON".encode())
 #print("RootCamONRootCamON") # Print statement instead of serial call, since FarmBot appears to have removed pyserial again...
 
 device.log(message='Trigger Photo Farmware run!', message_type='success')
